458d3491cf2d086f68579fa345b0f8a62053e019
rodric74 <rodric.rambosson@gmail.com>
2023-10-18 14:07:54 +0200

    ronds suivent souris

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+window.addEventListener("mousemove", (e) => {
+  //   console.log(e.x);
+  cursor.style.top = e.y + "px";
+  cursor.style.left = e.x + "px";
+
+  mouse1.style.top = e.y + "px";
+  mouse1.style.left = e.x + "px";
+
+  mouse2.style.top = e.y + "px";
+  mouse2.style.left = e.x + "px";
+});
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
 
