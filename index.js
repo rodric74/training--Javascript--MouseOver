c3bb56fbd719f7e8a99e5afb9e6935f75c2c0768
rodric74 <rodric.rambosson@gmail.com>
2023-10-18 14:22:01 +0200

    optim code js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+const mouses = document.querySelectorAll(".mouse");
+// console.log(mouses);
 window.addEventListener("mousemove", (e) => {
-  //   console.log(e.x);
-  cursor.style.top = e.y + "px";
-  cursor.style.left = e.x + "px";
-
-  mouse1.style.top = e.y + "px";
-  mouse1.style.left = e.x + "px";
-
-  mouse2.style.top = e.y + "px";
-  mouse2.style.left = e.x + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
 });
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+// window.addEventListener("mousemove", (e) => {
+//   //   console.log(e.x);
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
 
-// S'assurer que les liens sont clickables
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
 
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// });
